9a2a04b3782478761dc9f59f6878e890396676d1
codeWorm2015 <liu_rui_long@163.com>
2015-09-10 12:08:12 +0800

    Initial Commit

---
+++ LRLAVPalyer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LRLAVPalyer
+//
+//  Created by 刘瑞龙 on 15/9/10.
+//  Copyright (c) 2015年 V1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LRLAVPalyer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LRLAVPalyer
+//
+//  Created by 刘瑞龙 on 15/9/10.
+//  Copyright (c) 2015年 V1. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
