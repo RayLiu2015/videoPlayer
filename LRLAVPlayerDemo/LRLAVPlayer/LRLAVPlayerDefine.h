16b8ffbb86fb0fc98b2d9993b0de92038880fc64
codeWorm <liu_rui_long@163.com>
2016-04-19 00:08:25 +0800

    补充提交

--- LRLAVPlayerDemo/LRLAVPlayer/LRLAVPlayerDefine.h
+++ LRLAVPlayerDemo/LRLAVPlayer/LRLAVPlayerDefine.h
@@ -14,23 +14,26 @@
 #define TotalScreenTime 90
 #define LeastDistance 15
 
+//判断是否为ipad设备
+#define IsIpad ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad)
+
 //获取到window
 #define Window [[UIApplication sharedApplication].delegate window]
 
 //用来标记是否调试模式
-#define AvplayerDebug //
+//#define AvplayerDebug //
 
 #ifdef DEBUG
 
 //根据是否为调试模式, 来开启打印
 #ifdef AvplayerDebug
-#define Log(看我的是大SB, ...) NSLog((@"函数名: %s [行: %d]" 看我的是大SB), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define AVDLog(content, ...) NSLog((@"函数名: %s [行: %d]" content), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
 #else
-#define Log(...) //
+#define AVDLog(...) //
 #endif
 
 #else
-#define Log(...) //
+#define AVDLog(...) //
 #endif
 
 #define iOS8 [UIDevice currentDevice].systemVersion.floatValue >= 8.0
